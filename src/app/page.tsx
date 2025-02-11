76f5852e03e30787d05298d62a6706c2087f8a5d
jon-jew <jonathan.jew@gmail.com>
2025-02-11 13:31:17 -0500

    resolved-issues-image-gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,42 @@
 "use client"
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 const API_URL = 'https://api.unsplash.com/photos';
 
+interface Photo {
+  id: string,
+  urls: { small: string },
+  alt_description: string,
+};
 
 export default function Home() {
   const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     fetch(`${API_URL}?client_id=${process.env['NEXT_PUBLIC_ACCESS_KEY']}`)
-    .then((res) => res.json())
-    .then((photoList) => setPhotos(photoList))
+      .then((res) => res.json())
+      .then((photoList) => {
+        setPhotos(photoList);
+      });
   }, []);
 
-  console.log(photos);
   return (
-    <div className="image-gallery">
-      {photos.map((photo) => 
-        <img src={photo.links.download} alt={photo.altDescription} />
-      )}
+    <div className="app">
+      <h1>Image Gallery</h1>
+      <div className="image-gallery">
+        {photos.map((photo: Photo) =>
+          <div key={photo.id} className="image-container">
+            <Image
+              src={photo.urls.small}
+              alt={photo.alt_description}
+              loading="lazy"
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
